45ccb133b343647a1a7f234b9f8ddfa497339593
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-03-27 05:18:25 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -7,7 +7,7 @@ def pretty_print_nds(nds)
 pp nds
 end
 
-def print_first_directors_movie_titles(dir)
+def print_first_directors_movie_titles
 counter = 0 
 dir = directors_database[0][:movies]
 while counter < dir.length do 
